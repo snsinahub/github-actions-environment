917bde39631acab5559837bd849d9c0777a33cfe
Siavash Namvar <snsinahub@github.com>
2022-09-01 16:49:34 -0400

    first push

---
+++ src/main/java/com/snsinahub/envbased/EnvBasedApplication.java
@@ -0,0 +1,13 @@
+package com.snsinahub.envbased;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnvBasedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnvBasedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/snsinahub/envbased/EnvBasedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snsinahub.envbased;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnvBasedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
